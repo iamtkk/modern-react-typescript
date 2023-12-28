8f258517cafc5b09ff9e2a66f1d599068a601259
김태관 <iamtk@neowiz.com>
2023-12-29 07:47:27 +0900

    ch03_3 진행중

---
+++ ch03/ch03_3/src/pages/PaddingTest.tsx
@@ -0,0 +1,10 @@
+import {Title} from '../components'
+
+export default function PaddingTest() {
+  return (
+    <section className="mt-4">
+      <Title>PaddingTest</Title>
+      <div className="mt-4"></div>
+    </section>
+  )
+}
