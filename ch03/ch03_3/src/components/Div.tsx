2bb2d9ac7fc1c17f57d82142af8b29ad60241738
김태관 <iamtk@neowiz.com>
2023-12-30 13:26:10 +0900

    ch03_3

--- ch03/ch03_3/src/components/Div.tsx
+++ ch03/ch03_3/src/components/Div.tsx
@@ -1,12 +1,15 @@
 import type {FC, DetailedHTMLProps, HTMLAttributes, PropsWithChildren} from 'react'
 import type {WidthHeight} from './WidthHeight'
+import type {LeftRightTopBottom} from './LeftRightTopBottom'
 
 export type ReactDivProps = DetailedHTMLProps<
   HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 >
 
-export type DivProps = ReactDivProps & PropsWithChildren<WidthHeight> & {src?: string}
+export type DivProps = ReactDivProps &
+  PropsWithChildren<WidthHeight> &
+  LeftRightTopBottom & {src?: string}
 
 export const Div: FC<DivProps> = ({
   width,
@@ -14,14 +17,22 @@ export const Div: FC<DivProps> = ({
   style: _style,
   src,
   className: _className,
+  left,
+  right,
+  top,
+  bottom,
   ...props
 }) => {
   const style = {
     ..._style,
     width,
     height,
-    backgroundImage: src ? `url(${src})` : undefined
+    backgroundImage: src ? `url(${src})` : undefined,
+    left,
+    right,
+    top,
+    bottom
   }
-  const className = [src && 'bg-gray-300', _className].join(' ')
+  const className = ['box-sizing', src && 'bg-gray-300', _className].join(' ')
   return <div {...props} className={className} style={style} />
 }
