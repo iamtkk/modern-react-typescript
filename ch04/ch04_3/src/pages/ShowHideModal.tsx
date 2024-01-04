e52ed0ea714e5231861ff2f5cf674ceb18cd135e
김태관 <iamtk@neowiz.com>
2024-01-04 20:22:42 +0900

    ch04_4

--- ch04/ch04_3/src/pages/ShowHideModal.tsx
+++ ch04/ch04_3/src/pages/ShowHideModal.tsx
@@ -1,8 +1,39 @@
+import {useCallback} from 'react'
+import {Title, Subtitle} from '../components'
+import {Modal, ModalContent, ModalAction, Button} from '../theme/daisyui'
+import {useToggle} from '../hooks'
+import * as D from '../data'
+
 export default function ShowHideModal() {
+  const [open, toggleOpen] = useToggle(false)
+  const onAccept = useCallback(() => {
+    toggleOpen()
+  }, [toggleOpen])
+
   return (
     <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">ShowHideModal</h2>
-      <div className="mt-4"></div>
+      <Title>ShowHideModal</Title>
+      <div className="flex justify-center p-4">
+        <Button className="btn-primary" onClick={toggleOpen}>
+          open modal
+        </Button>
+      </div>
+      <Modal open={open}>
+        <ModalContent
+          closeIconClassName="btn-primary btn-outline"
+          onCloseIconClicked={toggleOpen}>
+          <Subtitle>Modal</Subtitle>
+          <p>{D.randomParagraph()}</p>
+          <ModalAction>
+            <Button className="btn btn-primary" onClick={onAccept}>
+              Accept
+            </Button>
+            <Button className="btn" onClick={toggleOpen}>
+              Close
+            </Button>
+          </ModalAction>
+        </ModalContent>
+      </Modal>
     </section>
   )
 }
