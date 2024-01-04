ff2f260f160bbb0863dd7eb85eae38e6bcba8674
김태관 <iamtk@neowiz.com>
2024-01-05 07:53:38 +0900

    1

--- ch04/ch04_3/src/pages/HigherOrderRadioInputTest.tsx
+++ ch04/ch04_3/src/pages/HigherOrderRadioInputTest.tsx
@@ -1,4 +1,14 @@
+import {useMemo, useCallback, useState} from 'react'
+import {Title, Subtitle} from '../components'
+import * as D from '../data'
+
 export default function HigherOrderRadioInputTest() {
+  const jobTitles = useMemo(() => D.makeArray(4).map(D.randomJobTitle), [])
+  const [selectedIndex, setSelectedIndex] = useState<number>(0)
+  const onChange = useCallback(
+    (index: number) => () => setSelectedIndex(notUsed => index),
+    []
+  )
   return (
     <section className="mt-4">
       <h2 className="font-bold text-5xl text-center">HigherOrderRadioInputTest</h2>
