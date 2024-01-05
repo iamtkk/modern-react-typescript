cc05a4785aa2b73b1dcdfe7705599a4a8411af36
김태관 <iamtk@neowiz.com>
2024-01-06 08:31:25 +0900

    1

--- ch04/ch04_3/src/pages/ObjectState.tsx
+++ ch04/ch04_3/src/pages/ObjectState.tsx
@@ -1,8 +1,71 @@
+import type {FormEvent, ChangeEvent} from 'react'
+import {useState, useCallback} from 'react'
+import {Title} from '../components'
+
+type FormType = {
+  name: string
+  email: string
+}
+
 export default function ObjectState() {
+  const [form, setForm] = useState<FormType>({name: '', email: ''})
+
+  const onSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      alert(JSON.stringify(form, null, 2))
+    },
+    [form]
+  )
+
+  const onChangeName = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setForm(state => ({...state, name: e.target.value}))
+  }, [])
+  const onChangeEmail = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setForm(form => ({...form, email: e.target.value}))
+  }, [])
+
   return (
     <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">ObjectState</h2>
-      <div className="mt-4"></div>
+      <Title>BasicForm</Title>
+      <div className="flex justify-center mt-4">
+        <form onSubmit={onSubmit}>
+          <div className="form-contrl">
+            <label className="label" htmlFor="name">
+              <span className="label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              value={form.name}
+              onChange={onChangeName}
+              id="name"
+              placeholder="enter your name"
+              className="input input-primary"
+            />
+          </div>
+          <div className="form-contrl">
+            <label className="label" htmlFor="email">
+              <span className="label-text">email</span>
+            </label>
+            <input
+              type="email"
+              value={form.email}
+              onChange={onChangeEmail}
+              id="email"
+              placeholder="enter your email"
+              className="input input-primary"
+            />
+          </div>
+          <div className="flex justify-center mt-4">
+            <input
+              type="submit"
+              value="Submit"
+              className="w-1/2 btn btn-sm btn-primary"
+            />
+            <input defaultValue="Cancel" className="w-1/2 ml-4 btn btn-sm" />
+          </div>
+        </form>
+      </div>
     </section>
   )
 }
