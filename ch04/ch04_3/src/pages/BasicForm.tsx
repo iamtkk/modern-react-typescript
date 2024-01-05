de87597ee69377c06008cf6a3ce607adea82c98f
김태관 <iamtk@neowiz.com>
2024-01-05 18:29:27 +0900

    basci form

--- ch04/ch04_3/src/pages/BasicForm.tsx
+++ ch04/ch04_3/src/pages/BasicForm.tsx
@@ -1,8 +1,70 @@
+import type {FormEvent, ChangeEvent} from 'react'
+import {useState, useCallback} from 'react'
+import {Title} from '../components'
+
 export default function BasicForm() {
+  const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+
+  const onSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      const formData = new FormData()
+      formData.append('name', name)
+      formData.append('email', email)
+      alert(JSON.stringify(Object.fromEntries(formData), null, 2))
+    },
+    [name, email]
+  )
+
+  const onChangeName = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setName(notUsed => e.target.value)
+  }, [])
+  const onChangeEmail = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(notUsed => e.target.value)
+  }, [])
+
   return (
     <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">BasicForm</h2>
-      <div className="mt-4"></div>
+      <Title>BasicForm</Title>
+      <div className="flex justify-center mt-4">
+        <form onSubmit={onSubmit}>
+          <div className="form-contrl">
+            <label className="label" htmlFor="name">
+              <span className="label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              value={name}
+              onChange={onChangeName}
+              id="name"
+              placeholder="enter your name"
+              className="input input-primary"
+            />
+          </div>
+          <div className="form-contrl">
+            <label className="label" htmlFor="email">
+              <span className="label-text">email</span>
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={onChangeEmail}
+              id="email"
+              placeholder="enter your email"
+              className="input input-primary"
+            />
+          </div>
+          <div className="flex justify-center mt-4">
+            <input
+              type="submit"
+              value="Submit"
+              className="w-1/2 btn btn-sm btn-primary"
+            />
+            <input defaultValue="Cancel" className="w-1/2 ml-4 btn btn-sm" />
+          </div>
+        </form>
+      </div>
     </section>
   )
 }
