6fea148b6e38904749f62ebfe8d87a25c8de2be7
김태관 <iamtk@neowiz.com>
2024-01-08 19:11:33 +0900

    1

--- ch04/ch04_5/src/pages/FileDrop.tsx
+++ ch04/ch04_5/src/pages/FileDrop.tsx
@@ -1,4 +1,40 @@
+import type {ChangeEvent, DragEvent} from 'react'
+import {useState, useRef, useCallback, useMemo} from 'react'
+import {useToggle} from '../hooks'
+import {Title, Div} from '../components'
+import {imageFileReaderP} from '../utils'
+
 export default function FileDrop() {
+  const [imageUrls, setImageUrls] = useState<string[]>([])
+  const [error, setError] = useState<Error | null>(null)
+  const [loading, toggleLoading] = useToggle(false)
+
+  const inputRef = useRef<HTMLInputElement>(null)
+  const onDivClick = useCallback(() => inputRef.current?.click(), [])
+
+  const makeImageUrls = useCallback(
+    (files: File[]) => {
+      const promises = Array.from(files).map(imageFileReaderP)
+      toggleLoading()
+      Promise.all(promises)
+        .then(urls => setImageUrls(imageUrls => [...urls, ...imageUrls]))
+        .catch(setError)
+        .finally(toggleLoading)
+    },
+    [toggleLoading]
+  )
+
+  const onInputChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setError(null)
+      const files = e.target.files
+      files && makeImageUrls(Array.from(files))
+    },
+    [makeImageUrls]
+  )
+
+  const images = useMemo(() => imageUrls.map(() => {}), [])
+
   return (
     <section className="mt-4">
       <h2 className="font-bold text-5xl text-center">FileDrop</h2>
