59a880757e7eb2b1882870c8d6ed165026279d39
김태관 <iamtk@neowiz.com>
2024-01-09 22:04:06 +0900

    5

--- ch04/ch04_5/src/theme/daisyui/ValidatableInput.tsx
+++ ch04/ch04_5/src/theme/daisyui/ValidatableInput.tsx
@@ -1,12 +1,37 @@
 import type {ReactInputProps} from './Input'
-import {forwardRef, useImperativeHandle, useMemo} from 'react'
+import {forwardRef, useImperativeHandle, useMemo, useRef} from 'react'
 
-export type ValidatableInputMethods = {}
+export type ValidatableInputMethods = {
+  validate: () => [boolean, string]
+}
 
 export const ValidatableInput = forwardRef<ValidatableInputMethods, ReactInputProps>(
-  ({className: _className, ...inputProps}, methodsRef) => {
+  ({type, className: _className, ...inputProps}, methodsRef) => {
     const className = useMemo(() => ['input', _className].join(' '), [_className])
-    useImperativeHandle(methodsRef, () => ({}), [])
-    return <input {...inputProps} className={className} />
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    useImperativeHandle(
+      methodsRef,
+      () => ({
+        validate: (): [boolean, string] => {
+          const value = inputRef.current?.value
+          if (!value || !value.length) return [false, '사용자가 입력한 내용이 없습니다.']
+
+          switch (type) {
+            case 'email': {
+              const regex =
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i
+              const valid = regex.test(value)
+              return valid ? [true, value] : [false, '틀린 이메일 주소입니다.']
+            }
+          }
+
+          return [true, value]
+        }
+      }),
+      [type]
+    )
+
+    return <input {...inputProps} className={className} ref={inputRef} />
   }
 )
